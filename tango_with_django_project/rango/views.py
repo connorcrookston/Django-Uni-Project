4ee1a3c64110f8ba9347ea3cce01fad90c082285
connorcrookston <connorghcrookston@gmail.com>
2022-03-10 17:34:59 +0000

    fixed bug with about path not reversing.

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<a href='/rango/about/'>About</a>")
+    return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
 
 def about(request):
-    return HttpResponse("<a href='/rango/'>Go Back!</a>")
+    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
