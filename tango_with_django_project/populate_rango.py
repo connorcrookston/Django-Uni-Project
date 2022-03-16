3e4f5ef6ed0f54c242c0934e4bcee5a24efb3b14
connorcrookston <connorghcrookston@gmail.com>
2022-03-16 15:58:35 +0000

    up to chapter 6 completed

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -11,7 +11,7 @@ def populate():
     python_pages = [
         {'title': 'Official Python Tutorial',
          'url': 'http://docs.python.org/3/tutorial/'},
-        {'title': 'Hot to Think Like a Computer Scientist',
+        {'title': 'How to Think like a Computer Scientist',
          'url': 'http://www.greenteapress.com/thinkpython/'},
         {'title': 'Learn Python in 10 Minutes',
          'url': 'http://www.koronkithankis.net/tutorials/python/'} ]
@@ -30,12 +30,12 @@ def populate():
         {'title': 'Flask',
          'url': 'http://flask.pocoo.org'} ]
 
-    cats = {'Python': {'pages': python_pages},
-            'Django': {'pages': django_pages},
-            'Other Frameworks': {'pages': other_pages}}
+    cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
+            'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
+            'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16}}
 
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat, cat_data['views'], cat_data['likes'])
         for p in cat_data['pages']:
             add_page(c, p['title'], p['url'])
 
@@ -52,8 +52,10 @@ def add_page(cat, title, url, views=0):
     return p
 
 
-def add_cat(name):
+def add_cat(name, views=0, likes=0):
     c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
     c.save()
     return c
 
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,17 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Category, Page
 
 def index(request):
     # Construct a dictionary to pass to the template engine as its context
     # note the key boldmessage matches to {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+
+    category_list = Category.objects.order_by('-likes')[:5]
+
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
 
     # return a rendered response to send to the client
     # we make use of the shortcut function to make our lives easier
     # note that the first parameter is the tempate we wish to use
     return render(request, 'rango/index.html', context=context_dict)
 
+
 def about(request):
     context_dict = {'name': 'Connor'}
     return render(request, 'rango/about.html', context=context_dict)
+
+
+def show_category(request, category_name_slug):
+    context_dict = []
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context=context_dict)
+
 
